f9e80abeaaabe4f7df3bc9eac5670354379dc00b
mbaharip <main@mbaharip.me>
2022-09-19 05:28:17 +0700

    Update blogs page to see if api-server would update

--- apps/web-portfolio/pages/blogs.tsx
+++ apps/web-portfolio/pages/blogs.tsx
@@ -7,8 +7,15 @@ import HeaderLayout from '../components/Layout/HeaderLayout';
 const Home: NextPage = () => {
 	return (
 		<MainLayout>
-			<HeaderLayout>Lorem ipsum</HeaderLayout>
-			<div className={`my-6 h-fit md:my-8`}>Lorem</div>
+			<HeaderLayout>Header Layout</HeaderLayout>
+			<div className={`my-6 h-fit md:my-8`}>
+				Page content goes here. Lorem ipsum dolor sit amet consectetur
+				adipisicing elit. Recusandae cupiditate, beatae sint consequatur
+				quisquam quia similique error nobis doloremque quidem modi ab eum
+				dolorem excepturi eligendi facere esse aspernatur voluptatum provident
+				libero eos nesciunt laboriosam totam. Magnam dolor debitis amet earum
+				porro. Laborum at temporibus obcaecati alias ad quasi id?
+			</div>
 		</MainLayout>
 	);
 };
