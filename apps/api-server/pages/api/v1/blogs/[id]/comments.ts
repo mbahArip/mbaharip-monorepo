e9babd82edcde09908848f6c58a2308987032923
mbaharip <main@mbaharip.me>
2022-10-24 23:43:39 +0700

    🐞 fix: Remove comment endpoint

--- apps/api-server/pages/api/v1/blogs/[id]/comments.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import postCommentHandler from '../../../../../handler/comments/postCommentHandler';
-import useMiddlewares from '../../../../../hooks/useMiddlewares';
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	await useMiddlewares(req, res);
-	await postCommentHandler(req, res);
-};
-export default handler;
