3aba998ce233562458be10278542a4dfb0708c38
Arief Rachmawan <62494292+mbahArip@users.noreply.github.com>
2024-07-09 13:41:47 +0700

    Add missing scheduler

--- apps/api-server/handler/scheduler/hoyoHandler.ts
+++ apps/api-server/handler/scheduler/hoyoHandler.ts
@@ -57,6 +57,7 @@ export default async function hoyoLogin(req:NextApiRequest, res: NextApiResponse
                 title: `✅ ${gameName[game]} check in success`,
                 description: `Successfully checked in to ${gameName[game]}`,
                 timestamp: new Date(),
+                footerText: "Scheduler"
                 fields: [],
             });
         } else if(res.message.includes('already') || res.retcode === -5003) {
@@ -64,6 +65,7 @@ export default async function hoyoLogin(req:NextApiRequest, res: NextApiResponse
                 title: `✅ ${gameName[game]} already checked in for today`,
                 description: `Check in to ${gameName[game]} aborted because you already checked in for today`,
                 timestamp: new Date(),
+                footerText: "Scheduler"
                 fields: [],
             });
         } else {
@@ -74,6 +76,7 @@ export default async function hoyoLogin(req:NextApiRequest, res: NextApiResponse
             title: `❌ ${gameName[game]} check in failed`,
             description: `Failed checked in to ${gameName[game]}\nError: ${error.message}`,
             timestamp: new Date(),
+            footerText: "Scheduler"
             fields: [],
         });
     }
